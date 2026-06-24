ccfdc9824af1d9538035c3a5af7df2d822419fc9
sunny1999123 <590913ab@eur.nl>
2026-06-24 10:36:56 +0200

    Fix voor deployment

--- .github/workflows/vite.config.js
+++ .github/workflows/vite.config.js
@@ -1,9 +1,10 @@
+Vite.config · JS
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
  
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: command === 'build' ? '/liftbi/' : '/',
-}))
+  base: process.env.GITHUB_ACTIONS ? '/liftbi/' : '/',
+})
  
